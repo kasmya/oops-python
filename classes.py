f3551fb273974c5ec62cccf0151fe6ed2980ba6e
kasmya bhatia <109264892+kasmya@users.noreply.github.com>
2026-06-08 17:21:36 +0530

    Encapsulate brand in Car class and add ElectricCar subclass
    Refactor Car class to encapsulate brand attribute and add getter methods. Introduce ElectricCar class that inherits from Car.

--- classes.py
+++ classes.py
@@ -1,13 +1,38 @@
 # class and object
 # create car class with attributes(variables) like brand and model. then create an instance of that class
 class Car:
-  def __init__(self, brand, model): #self refers to whoever calls the function/context
-      self.brand = brand #self.brand refers to the variables within the class, whereas brand model refers to paramaters passed by the user
+  def __init__(self, brand, model): #self refers to whoever calls the function/context, __init__ function is known as a  constructor
+      self.__brand = brand #self.brand refers to the variables within the class, whereas brand model refers to paramaters passed by the user
       self.model = model
 
+  def get_brand(self):
+    return self.__brand + " !"
+    
+  # class method and self 
+  # add a method to car which displays the full name of the car the model and the name
+
+  def full_name(self):
+    return f"{self.__brand}{self.model}" #formatted string
+
+#Inheritance 
+# create an electric car class that oinherits from car class and has an additional attribute battery_size 
+class ElectricCar(Car): #inherits proprties of car class ie brand and model
+  def __init__(self, brand, model, battery_size):
+    super().__init__(brand, model) 
+    self.battery_size = battery_size
+  
+my_tesla = ElectricCar("tesla", "model s", "85kWH")
+print(my_tesla.model)
+print(my_tesla.get_brand())
+
 my_car = Car("toyota", "corolla") #object 
-print(my_car.brand)
+print(my_car.__brand)
 print(my_car.model)
 my_new_car = Car("tata", "safari")
-print(my_new_car.model)
+print(my_new_car.model) #class defination hence no paranthesis needed
 # generalised form, usually the class is created in a seperate file imported whenever it is to be used 
+
+print(my_new_car.full_name()) #defined functions adds functionality, hence paranthesis should be added to the function call
+
+#Encapsulation
+#modify car class to encapsulate the brand attribute, making it private and provide a getter method for it 
