e7b28201a5f96e3df5dede4ccb2ea30a6bd75e09
kasmya bhatia <109264892+kasmya@users.noreply.github.com>
2026-06-10 10:18:21 +0530

    Enhance comment for static method in Student class
    Added a comment to explain the purpose of the static method decorator.

--- static_methods.py
+++ static_methods.py
@@ -1,7 +1,9 @@
 #static methods - methods that don't use the self parameter (work at class level)
 class Student:
-  @staticmethod
+  @staticmethod #converts a normal function to be a static method
   def college():
     print("ABC college")
 
 #decorators allow us to wrap another function in order to extend behavior of the wrapped function, without permanently modifying it
+
+
