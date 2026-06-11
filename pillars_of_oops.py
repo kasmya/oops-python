92946b9c63e26917a7c3027414d7ca49c106bf69
kasmya bhatia <109264892+kasmya@users.noreply.github.com>
2026-06-11 13:03:57 +0530

    Enhance pillars_of_oops.py with inheritance examples
    Added examples of inheritance and polymorphism in Python.

--- pillars_of_oops.py
+++ pillars_of_oops.py
@@ -48,7 +48,74 @@ print(account.get_balance())   # Access via method
 account.deposit(500)
 print(account.get_balance())
 
-# print(account.__balance)    Error: Attribute not directly accessible
+# print(account.__balance)    Error: Attribute not directly accessible by user but can be accessed by internal functions in the class
+# done to prevent exposing instance attributes outside the class
 
-# 3. INHERITANCE
+# example:
+class Person():
+    __name = "kas" #private attribute
+
+ def __hello(self): 
+    print("hello person")
+
+ def welcome(self):
+   self.__hello()
+
+p1 = Person()
+print(p1.welcome())
+
+# 3. INHERITANCE - when one class derives properties and methods of another class 
+# inherits logic from another class, reduces rewriting of code
+class Car():
+  def __init__(self,type):
+    self.type = type
+  
+  color = "pink"
+  @staticmethod
+  def start():
+    print("car is starting...")
+
+  @staticmethod
+  def stop():
+    print("car is stopping...")
+
+class ToyotaCar(Car): #ToyotaCar inherits from Car class, single level
+  def __init__(self, brand):
+    #self.name = name
+    self.brand = brand
+
+class Fortuner(ToyotaCar): #multi level inheritance 
+  def __init__(self):
+    self.type = type
+    super().__init__(type) #super method - used to access methods of the parent class 
+    super.start() 
+
+car2 = Fortuner("diesel")
+car2.start()
+  
+print(car2.type)
+
+#car1=ToyotaCar("fortuner")
+#print(car1.start())
+#print(car1.color)
+
+#types of inheritance
+#1. single inheritance - single parent/base class gives single child/derived class 
+#2. multi level inheritance - parent class - child class (parent to next class) - grandchild (child to preceding class, containing properties of both preceding classes) 
+#3. multiple inheritance - child/derived class can inherit properties from multiple parent classes
+
+class A:
+  varA = "welcome to class A"
+
+class B:
+  varB = "welcome to class B"
+
+class C(A,B): #multiple inheritance
+  varC = "welcome to class C"
+
+c1 = C()
+print(c1.varA)
+print(c1.varB)
+print(c1.varC)
+
 # 4. POLYMORPHISM 
